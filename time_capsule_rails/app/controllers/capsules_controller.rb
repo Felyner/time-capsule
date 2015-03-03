ebeae8b4ef963862d7146bd121599f6dd6fd6679
Patrick Mathieu <pdmathieu5@gmail.com>
2015-03-03 00:59:37 -0800

    Just pushing updates. Trying to create new capsules.

--- time_capsule_rails/app/controllers/capsules_controller.rb
+++ time_capsule_rails/app/controllers/capsules_controller.rb
@@ -1,7 +1,24 @@
 class CapsulesController < ApplicationController
 
+	before_filter :authenticate_user!
+
 	def index
 		@capsules = Capsule.all
+	end
+
+	def new
+		@capsule = Capsule.new
+		@items = Item.new
+	end
+
+	def create
+		@capsule = Capsule.new(params[:f])
+		@items = Item.new(params[:i])
+		if @capsule.save
+			redirect_to capsules_path, :notice => "Your time capsule has been locked away"
+		else
+			render "new"
+		end
 	end
 
 end
